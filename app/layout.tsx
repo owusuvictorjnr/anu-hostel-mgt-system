c63ea24799c3d068e2c7c1bb266796b89c9087ca
owusuvictorjnr <owusuvictor80@gmail.com>
2024-03-04 22:07:05 +0000

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+  style: ['italic', 'normal'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: 'Hostel Management',
+  description: 'All Nations Hostel Management',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
