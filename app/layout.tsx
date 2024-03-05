9f7ebb47b8fd98512297b94ddb44665fb566ff7e
owusuvictorjnr <owusuvictor80@gmail.com>
2024-03-05 11:36:46 +0000

    wrap the main with ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
+import ThemeProvider from '@/components/ThemeProvider/ThemeProvider'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -24,13 +25,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className=" font-normal">
-          {/* Header */}
-          <Header />
-          {children}
-          {/* Footer */}
-          <Footer />
-        </main>
+        <ThemeProvider>
+          <main className=" font-normal">
+            {/* Header */}
+            <Header />
+            {children}
+            {/* Footer */}
+            <Footer />
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
