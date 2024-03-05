b996311b0c614457d2e2f78bd266af9e44c5a9a5
owusuvictorjnr <owusuvictor80@gmail.com>
2024-03-05 08:03:10 +0000

    import header and footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/Header'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className=" font-normal">
+          {/* Header */}
+          <Header />
+          {children}
+          {/* Footer */}
+        </main>
+      </body>
     </html>
   )
 }
