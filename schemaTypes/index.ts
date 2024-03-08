3c4b7a0d1a5851352e9dffcaa234b87c123cc014
owusuvictorjnr <owusuvictor80@gmail.com>
2024-03-08 09:45:31 +0000

    fields schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import user from './user'
+import account from './account'
+import booking from './booking'
+import hostelRoom from './hostelRoom'
+import review from './review'
+import verificationToken from './verificationToken'
+
+export const schemaTypes = [
+  user,
+  account,
+  booking,
+  hostelRoom,
+  review,
+  verificationToken,
+]
